c1879575c18696a830b6df732c83031dbe9c0924
ah-naf <81911439+ah-naf@users.noreply.github.com>
2021-04-04 19:19:33 +0600

    Create ABSYS.cpp

---
+++ ABSYS.cpp
@@ -0,0 +1,99 @@
+/*________________________Bismillahir Rahmanir Rahim___________________
+|                                                                      |
+|                    /\     |     | |\    |     /\     |-------        |
+|                   /  \    |     | | \   |    /  \    |               |
+|                  /----\   |-----| |  \  |   /----\   |-----          |
+|                 /      \  |     | |   \ |  /      \  |               |
+|                /        \ |     | |    \| /        \ |               |
+|                     14th batch CSE , ID : 12008029                   |
+|                     Comilla University                               |
+|                     Email : sheikhahnafshifat@gmail.com              |
+|______________________________________________________________________*/
+#include<bits/stdc++.h>
+#include<string>
+#include<iostream>
+using namespace std;
+#define ll                      long long
+#define MAX                     1000007
+#define pb(x)                   push_back(x)
+#define mp(x,y)                 make_pair(x,y)
+#define vii                     vector<int>
+#define vll                     vector<long long>
+#define vp                      vector<pair<ll, ll> >
+#define v_min(a)                *min_element(a.begin(),a.end())
+#define v_max(a)                *max_element(a.begin(),a.end())
+#define v_sum(a)                accumulate(a.begin(),a.end(),0)
+#define un(a)                   a.erase(unique(a.begin(),a.end()),a.end())
+#define SORT(a)                 sort(a.begin(),a.end())
+#define AHNAF                   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define YES                     cout<<"YES"<<endl
+#define NO                      cout<<"NO"<<endl
+#define testcase(t)             int t;cin>>t;while(t--)
+#define reverse(a)              reverse(a.begin(),a.end())
+#define ff                      first
+#define ss                      second
+#define print(a)                for(auto it: a) cout<<it<<" "
+#define all(a)                  a.begin(),a.end()
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
+int main()
+{
+    AHNAF;int ca=0;
+    testcase(t)
+    {
+        string a,b,c,d,e;cin>>a>>b>>c>>d>>e;
+        bool cc=false;
+        for(int i=0; i<a.size(); i++) {
+            if(a[i]>='0' && a[i]<='9');
+            else {
+                cc=true;break;
+            }
+        }
+        vii num(3);
+        if(!cc) num[0]=stoi(a);
+        else num[0]=-1;
+        cc=false;
+        for(int i=0; i<c.size(); i++) {
+            if(c[i]>='0' && c[i]<='9');
+            else {
+                cc=true;break;
+            }
+        }
+        if(!cc) num[1]=stoi(c);
+        else num[1]=-1;
+        cc=false;
+        for(int i=0; i<e.size(); i++) {
+            if(e[i]>='0' && e[i]<='9');
+            else {
+                cc=true;break;
+            }
+        }
+        if(!cc) num[2]=stoi(e);
+        else num[2]=-1;
+        //print(num);cout<<endl;
+        if(num[2]==-1) {
+            cout<<num[0]<<" + "<<num[1]<<" = "<<num[0]+num[1]<<endl;
+        }
+        else {
+            if(num[0]==-1) {
+                num[0]=num[2]-num[1];
+            }
+            else {
+                num[1]=num[2]-num[0];
+            }
+            cout<<num[0]<<" + "<<num[1]<<" = "<<num[2]<<endl;
+        }
+    }
+
+}
+
+
