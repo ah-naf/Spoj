14165f94d7988c705b12bbc775faf762afb1b512
ah-naf <81911439+ah-naf@users.noreply.github.com>
2021-04-04 19:30:13 +0600

    Create HPYNOS.cpp

---
+++ HPYNOS.cpp
@@ -0,0 +1,66 @@
+/*________________________Bismillahir Rahmanir Rahim___________________
+|                                                                      |
+|                    /\     |     | |\    |     /\     |-------        |
+|                   /  \    |     | | \   |    /  \    |               |
+|                  /----\   |-----| |  \  |   /----\   |-----          |
+|                 /      \  |     | |   \ |  /      \  |               |
+|                /        \ |     | |    \| /        \ |               |
+|                     14th batch CSE , ID : 12008029                   |
+|                     Comilla University                               |
+|                     Email : sheikhahnafshifat@gmail.com              |
+|______________________________________________________________________*/
+#include<bits/stdc++.h>
+#include<string>
+#include<iostream>
+using namespace std;
+#define ll                      long long
+#define MAX                     1000007
+#define pb(x)                   push_back(x)
+#define mp(x,y)                 make_pair(x,y)
+#define vii                     vector<int>
+#define vll                     vector<long long>
+#define vp                      vector<pair<ll, ll> >
+#define v_min(a)                *min_element(a.begin(),a.end())
+#define v_max(a)                *max_element(a.begin(),a.end())
+#define v_sum(a)                accumulate(a.begin(),a.end(),0)
+#define un(a)                   a.erase(unique(a.begin(),a.end()),a.end())
+#define SORT(a)                 sort(a.begin(),a.end())
+#define AHNAF                   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define YES                     cout<<"YES"<<endl
+#define NO                      cout<<"NO"<<endl
+#define testcase(t)             int t;cin>>t;while(t--)
+#define reverse(a)              reverse(a.begin(),a.end())
+#define ff                      first
+#define ss                      second
+#define print(a)               for(auto it: a) cout<<it<<" "
+ll dig(ll n) {
+    ll sum=0;
+    while(n) {
+        sum+=((n%10)*(n%10));
+        n/=10;
+    }
+    return sum;
+}
+int main()
+{
+    AHNAF;int ca=0;
+    ll n;cin>>n;
+    set<ll> s;s.insert(n);
+    while(1) {
+        n=dig(n);
+        ca++;
+        if(s.count(n)) {
+            cout<<-1<<endl;
+            return 0;
+        }
+        if(n==1) {
+            cout<<ca<<endl;
+            return 0;
+        }
+        else {
+            s.insert(n);
+        }
+    }
+}
