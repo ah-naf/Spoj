510841c15ce2461839dce8d2f4f3b480064f992d
ah-naf <81911439+ah-naf@users.noreply.github.com>
2021-04-04 19:30:46 +0600

    Create BEENUMS.cpp

---
+++ BEENUMS.cpp
@@ -0,0 +1,93 @@
+/*________________________Bismillahir Rahmanir Rahim___________________
+|                                                                      |
+|                    /\     |     | |\    |     /\     |-------        |
+|                   /  \    |     | | \   |    /  \    |               |
+|                  /----\   |-----| |  \  |   /----\   |-----          |
+|                 /      \  |     | |   \ |  /      \  |               |
+|                /        \ |     | |    \| /        \ |               |
+|                     14th batch CSE , ID : 12008029                   |
+|                     Comilla University                               |
+|                     Email : sheikhahnafshifat@gmail.com              |
+|______________________________________________________________________*/
+#include<bits/stdc++.h>
+#include<string>
+#include<iostream>
+using namespace std;
+#define ll                      long long
+#define MAX                     1000007
+#define pb(x)                   push_back(x)
+#define mp(x,y)                 make_pair(x,y)
+#define vii                     vector<int>
+#define vll                     vector<long long>
+#define vp                      vector<pair<ll, ll> >
+#define v_min(a)                *min_element(a.begin(),a.end())
+#define v_max(a)                *max_element(a.begin(),a.end())
+#define v_sum(a)                accumulate(a.begin(),a.end(),0LL)
+#define un(a)                   a.erase(unique(a.begin(),a.end()),a.end())
+#define SORT(a)                 sort(a.begin(),a.end())
+#define AHNAF                   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define YES                     cout<<"YES"<<endl
+#define NO                      cout<<"NO"<<endl
+#define testcase(t)             int t;cin>>t;while(t--)
+#define reverse(a)              reverse(a.begin(),a.end())
+#define ff                      first
+#define ss                      second
+#define print(a)                for(auto it: a) cout<<it<<" "
+#define all(a)                  a.begin(),a.end()
+#define sf(v)                   for(auto &it: v) cin>>it
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+bool checkbit(int mask,int bit)
+{
+    return mask & (1<<bit);
+}
+int setbit(int mask,int bit)
+{
+    return mask | (1<<bit) ;
+}
+int clearbit(int mask,int bit)
+{
+    return mask & ~(1<<bit);
+}
+int togglebit(int mask,int bit)
+{
+    return mask ^ (1<<bit);
+}
+//--------------------------------------------------------------//
+
+int main() {
+	int n,flag;
+	while(cin>>n){
+		flag=0;
+		if(n==-1){
+			break;
+		}
+		int res=0,i=0;
+		while(res<n){
+			if(i==0){
+				res=1;
+			}
+			else
+			res+=6*i;
+			i++;
+			//cout<<res<<" ";
+			if(res==n){
+				cout<<"Y"<<endl;
+				flag=1;
+				break;
+			}
+		}
+		if(!flag )
+		cout<<"N"<<endl;
+		cout<<endl;
+	}
+	return 0;
+}
