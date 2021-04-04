896efdcc11969495820fbc547bb14e1f65af98db
ah-naf <81911439+ah-naf@users.noreply.github.com>
2021-04-04 19:37:48 +0600

    Create FACTCG2.cpp

---
+++ FACTCG2.cpp
@@ -0,0 +1,93 @@
+/*________________________Bismillahir Rahmanir Rahim___________________
+|                                                                      |
+|                    /\     |     | |\    |     /\     |-------        |
+|                   /  \    |     | | \   |    /  \    |               |
+|                  /----\   |-----| |  \  |   /----\   |-----          |
+|                 /      \  |     | |   \ |  /      \  |               |
+|                /        \ |     | |    \| /        \ |               |
+|                     14th batch CSE , ID : 12008029                   |
+|                     Comilla University                               |
+|                     Email : sheikhahnafshifat@gmail.com              |
+|______________________________________________________________________*/
+#include<bits/stdc++.h>
+#include<string>
+#include<iostream>
+using namespace std;
+#define ll                      long long
+#define MAX                     1000007
+#define pb(x)                   push_back(x)
+#define mp(x,y)                 make_pair(x,y)
+#define vii                     vector<int>
+#define vll                     vector<long long>
+#define vp                      vector<pair<ll, ll> >
+#define v_min(a)                *min_element(a.begin(),a.end())
+#define v_max(a)                *max_element(a.begin(),a.end())
+#define v_sum(a)                accumulate(a.begin(),a.end(),0)
+#define un(a)                   a.erase(unique(a.begin(),a.end()),a.end())
+#define SORT(a)                 sort(a.begin(),a.end())
+#define AHNAF                   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define YES                     cout<<"YES"<<endl
+#define NO                      cout<<"NO"<<endl
+#define testcase(t)             int t;cin>>t;while(t--)
+#define reverse(a)              reverse(a.begin(),a.end())
+#define ff                      first
+#define ss                      second
+bool prime[MAX];
+vector <int> v;
+
+void sieve ()
+{
+    int i,j;
+
+    prime[0] = prime[1] = true;
+    v.pb(2);
+
+    for (i=4; i<MAX; i+=2)
+        prime[i] = true;
+
+    for (i=3; i*i<=MAX; i+=2)
+        if (!prime[i])
+           for (j=i*i; j<MAX; j+=2*i)
+                prime[j] = true;
+
+    for (i=3; i<MAX; i+=2)
+        if (!prime[i])
+            v.pb(i);
+}
+
+int main ()
+{
+    AHNAF;
+    sieve ();
+    int n,i,len;
+    while (scanf("%d",&n) != EOF)
+    {
+        vii p;
+        p.pb(1);
+        for (i=0; v[i]*v[i]<=n; i++)
+        {
+            while (n % v[i] == 0)
+            {
+                p.pb(v[i]);
+                n /= v[i];
+            }
+        }
+        if (n > 1)
+            p.pb(n);
+        len = p.size();
+        for (i=0; i<len; i++)
+        {
+            printf("%d",p[i]);
+
+            if (i == len-1)
+                printf("\n");
+            else
+                printf(" x ");
+        }
+    }
+
+    return 0;
+}
+
