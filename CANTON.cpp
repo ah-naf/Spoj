4985d6fd936f6da1a393007f7e0a1562d9b861fa
ah-naf <81911439+ah-naf@users.noreply.github.com>
2021-04-04 19:24:18 +0600

    Create CANTON.cpp

---
+++ CANTON.cpp
@@ -0,0 +1,90 @@
+/*________________________Bismillahir Rahmanir Rahim___________________
+|                                                                      |
+|                    /\     |     | |\    |     /\     |-------        |
+|                   /  \    |     | | \   |    /  \    |               |
+|                  /----\   |-----| |  \  |   /----\   |-----          |
+|                 /      \  |     | |   \ |  /      \  |               |
+|                /        \ |     | |    \| /        \ |               |
+|                     14th batch CSE , ID : 12008029                   |
+|                     Comilla University                               |
+|                     Email : sheikhahnafshifat@gmail.com              |
+|______________________________________________________________________*/
+#include<bits/stdc++.h>
+#include<string>
+#include<iostream>
+using namespace std;
+#define ll                      long long
+#define MAX                     1000007
+#define pb(x)                   push_back(x)
+#define mp(x,y)                 make_pair(x,y)
+#define vii                     vector<int>
+#define vll                     vector<long long>
+#define vp                      vector<pair<ll, ll> >
+#define v_min(a)                *min_element(a.begin(),a.end())
+#define v_max(a)                *max_element(a.begin(),a.end())
+#define v_sum(a)                accumulate(a.begin(),a.end(),0)
+#define un(a)                   a.erase(unique(a.begin(),a.end()),a.end())
+#define SORT(a)                 sort(a.begin(),a.end())
+#define AHNAF                   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define YES                     cout<<"YES"<<endl
+#define NO                      cout<<"NO"<<endl
+#define testcase(t)             int t;cin>>t;while(t--)
+#define reverse(a)              reverse(a.begin(),a.end())
+#define ff                      first
+#define ss                      second
+#define print(a)                for(auto it: a) cout<<it<<" "
+#define all(a)                  a.begin(),a.end()
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
+int main()
+{
+    AHNAF;
+    testcase(t)
+    {
+        ll n;cin>>n;
+        int cnt;
+        for(cnt=1; ; cnt++)
+        {
+            ll tmp=(cnt*(cnt+1))/2;
+            if(tmp>=n) break;
+        }
+        ll term=cnt;
+        cnt--;
+        //cout<<cnt<<" "<<term<<endl;
+        if(term&1) {
+            ll tmp=(cnt*(cnt+1))/2;
+            //cout<<tmp<<endl;
+            tmp=abs(n-tmp);
+            //cout<<tmp<<endl;
+            ll x=1,y=term;tmp--;
+            while(tmp)
+            {
+               x++;y--;tmp--;
+            }
+            cout<<"TERM "<<n<<" IS "<<y<<"/"<<x<<endl;
+        }
+        else {
+            ll tmp=(cnt*(cnt+1))/2;
+            //cout<<tmp<<endl;
+            tmp=abs(n-tmp);
+            //cout<<tmp<<endl;
+            ll x=1,y=term;tmp--;
+            while(tmp)
+            {
+               x++;y--;tmp--;
+            }
+            cout<<"TERM "<<n<<" IS "<<x<<"/"<<y<<endl;
+        }
+    }
+}
+
+
