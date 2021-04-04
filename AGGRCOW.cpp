9e44253a8c959c62a9d6054628f345fab459d9a0
ah-naf <81911439+ah-naf@users.noreply.github.com>
2021-04-04 19:11:47 +0600

    Create AGGRCOW.cpp

---
+++ AGGRCOW.cpp
@@ -0,0 +1,117 @@
+/*________________________Bismillahir Rahmanir Rahim___________________
+|                                                                      |
+|                    /\     |     | |\    |     /\     |-------        |
+|                   /  \    |     | | \   |    /  \    |               |
+|                  /----\   |-----| |  \  |   /----\   |-----          |
+|                 /      \  |     | |   \ |  /      \  |               |
+|                /        \ |     | |    \| /        \ |               |
+|                     14th batch CSE , ID : 12008029                   |
+|                     Comilla University                               |
+|                     Email : sheikhahnafshifat@gmail.com              |
+|______________________________________________________________________*/
+#include<bits/stdc++.h>
+#include<string>
+#include<iostream>
+using namespace std;
+#define ll                      long long
+#define MAX                     1000007
+#define pb(x)                   push_back(x)
+#define mp(x,y)                 make_pair(x,y)
+#define vii                     vector<int>
+#define vll                     vector<long long>
+#define vp                      vector<pair<ll, ll> >
+#define v_min(a)                *min_element(a.begin(),a.end())
+#define v_max(a)                *max_element(a.begin(),a.end())
+#define v_sum(a)                accumulate(a.begin(),a.end(),0LL)
+#define un(a)                   a.erase(unique(a.begin(),a.end()),a.end())
+#define SORT(a)                 sort(a.begin(),a.end())
+#define AHNAF                   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define YES                     cout<<"YES"<<endl
+#define NO                      cout<<"NO"<<endl
+#define testcase(t)             int t;cin>>t;while(t--)
+#define reverse(a)              reverse(a.begin(),a.end())
+#define ff                      first
+#define ss                      second
+#define print(a)                for(auto it: a) cout<<it<<" "
+#define all(a)                  a.begin(),a.end()
+#define sf(v)                   for(auto &it: v) cin>>it
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+bool checkbit(int mask,int bit)
+{
+    return mask & (1<<bit);
+}
+int setbit(int mask,int bit)
+{
+    return mask | (1<<bit) ;
+}
+int clearbit(int mask,int bit)
+{
+    return mask & ~(1<<bit);
+}
+int togglebit(int mask,int bit)
+{
+    return mask ^ (1<<bit);
+}
+//--------------------------------------------------------------//
+
+int n,c;
+int func(int num,int array[])
+{
+    int cows=1,pos=array[0];
+    for (int i=1; i<n; i++)
+    {
+        if (array[i]-pos>=num)
+        {
+            pos=array[i];
+            cows++;
+            if (cows==c)
+                return 1;
+        }
+    }
+    return 0;
+}
+int bs(int array[])
+{
+    int ini=0,last=array[n-1],max=-1;
+    while (last>ini)
+    {
+        //cout<<last<<" "<<ini<<endl;
+        int mid=(ini+last)/2;
+        if (func(mid,array)==1)
+        {
+            //cout<<mid<<endl;
+            if (mid>max)
+                max=mid;
+            ini=mid+1;
+        }
+        else
+            last=mid;
+    }
+    return max;
+}
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while (t--)
+    {
+        scanf("%d %d",&n,&c);
+        int array[n];
+        for (int i=0; i<n; i++)
+            scanf("%d",&array[i]);
+        sort(array,array+n);
+        //cout<<" dfsa \n";
+        int k=bs(array);
+        printf("%d\n",k);
+    }
+    return 0;
+}
