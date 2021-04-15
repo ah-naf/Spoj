0737cf146dacdf23aae1f4a76213b5054db95160
Ahnaf Hasan Shifat <81911439+ah-naf@users.noreply.github.com>
2021-04-15 14:47:07 +0600

    Create ARRAYSUB - subarrays.cpp

---
+++ ARRAYSUB - subarrays.cpp
@@ -0,0 +1,77 @@
+/*________________________Bismillahir Rahmanir Rahim___________________
+|                                                                      |
+|                    /\     |     | |\    |     /\     |-------        |
+|                   /  \    |     | | \   |    /  \    |               |
+|                  /----\   |-----| |  \  |   /----\   |-----          |
+|                 /      \  |     | |   \ |  /      \  |               |
+|                /        \ |     | |    \| /        \ |               |
+|                     14th batch CSE , ID : 12008029                   |
+|                     Comilla University                               |
+|                     Email : sheikhahnafshifat@gmail.com              |
+|______________________________________________________________________*/
+#include<bits/stdc++.h>
+#include<string>
+#include<iostream>
+using namespace std;
+#define ll                      long long
+#define MAX                     1000007
+#define pb(x)                   push_back(x)
+#define mp(x,y)                 make_pair(x,y)
+#define vii                     vector<int>
+#define vll                     vector<long long>
+#define vp                      vector<pair<ll, ll> >
+#define v_min(a)                *min_element(a.begin(),a.end())
+#define v_max(a)                *max_element(a.begin(),a.end())
+#define v_sum(a)                accumulate(a.begin(),a.end(),0)
+#define un(a)                   a.erase(unique(a.begin(),a.end()),a.end())
+#define SORT(a)                 sort(a.begin(),a.end())
+#define AHNAF                   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define YES                     cout<<"YES"<<endl
+#define NO                      cout<<"NO"<<endl
+#define testcase(t)             int t;cin>>t;while(t--)
+#define reverse(a)              reverse(a.begin(),a.end())
+#define ff                      first
+#define ss                      second
+#define print(a)                for(auto it: a) cout<<it<<" "
+#define all(a)                  a.begin(),a.end()
+#define sf(v)                   for(auto &it: v) cin>>it
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
+//  Algo :- SQRT Decomposition
+
+int main()
+{
+    AHNAF;
+    int n;cin>>n; vii v(n); sf(v); int k;cin>>k;
+    int sq = (int)sqrt(n+.0)+1;
+    vii block(sq, INT_MIN);
+    for(int i=0; i<n; i++) {
+        block[i/sq] = max(block[i/sq], v[i]);
+    }
+    for(int l = 0 ; ;l++) {
+        int r = l + k - 1; int ans = INT_MIN;
+        if(r >= n) break;
+        for(int i = l; i <= r; ) {
+            if(i % sq == 0 && i + sq - 1 <= r) {
+                ans = max(ans, block[i/sq]);
+                i += sq;
+            }
+            else {
+                ans = max(ans, v[i]);
+                i++;
+            }
+        }
+        cout<<ans<<" ";
+    }
+    cout<<endl;
+}
+
