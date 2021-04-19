2a65535ec2a39e0b7d487f802d695a05c099a38a
Ahnaf Hasan Shifat <81911439+ah-naf@users.noreply.github.com>
2021-04-19 16:26:40 +0600

    Create ABCDEF - ABCDEF.cpp

---
+++ ABCDEF - ABCDEF.cpp
@@ -0,0 +1,76 @@
+/*________________________Bismillahir Rahmanir Rahim___________________
+                                                                      |
+|                    /\     |     | |\    |     /\     |-------        |
+|                   /  \    |     | | \   |    /  \    |               |
+|                  /----\   |-----| |  \  |   /----\   |-----          |
+|                 /      \  |     | |   \ |  /      \  |               |
+|                /        \ |     | |    \| /        \ |               |
+|                     14th batch CSE , ID : 12008029                   |
+|                     Comilla University                               |
+|                     Email : sheikhahnafshifat@gmail.com              |
+|______________________________________________________________________*/
+#include<bits/stdc++.h>
+#include<string>
+#include<iostream>
+using namespace std;
+#define ll                      long long
+#define MAX                     1000007
+#define pb(x)                   push_back(x)
+#define mp(x,y)                 make_pair(x,y)
+#define vii                     vector<int>
+#define vll                     vector<long long>
+#define vp                      vector<pair<ll, ll> >
+#define v_min(a)                *min_element(a.begin(),a.end())
+#define v_max(a)                *max_element(a.begin(),a.end())
+#define v_sum(a)                accumulate(a.begin(),a.end(),0)
+#define un(a)                   a.erase(unique(a.begin(),a.end()),a.end())
+#define SORT(a)                 sort(a.begin(),a.end())
+#define AHNAF                   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define YES                     cout<<"YES"<<endl
+#define NO                      cout<<"NO"<<endl
+#define testcase(t)             int t;cin>>t;while(t--)
+#define reverse(a)              reverse(a.begin(),a.end())
+#define ff                      first
+#define ss                      second
+#define print(a)                for(auto it: a) cout<<it<<" "
+#define all(a)                  a.begin(),a.end()
+#define sf(v)                   for(auto &it:v) cin>>it
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
+int main()
+{
+    AHNAF;
+    int n;cin>>n; vll v(n); sf(v);
+    vll v1, v2;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            for(int k=0; k<n; k++) {
+                v1.pb(v[i]*v[j]+v[k]);
+            }
+        }
+    }
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            for(int k=0; k<n; k++) {
+                if(v[i] == 0) continue;
+                v2.pb(v[i]*(v[j] + v[k]));
+            }
+        }
+    }
+    SORT(v1); SORT(v2); ll ans = 0;
+    for(int i=0; i<v1.size(); i++) {
+        int lo = lower_bound(all(v2), v1[i]) - v2.begin();
+        int hi = upper_bound(all(v2), v1[i]) - v2.begin();
+        ans += (hi - lo);
+    }
+    cout<<ans<<endl;
+}
