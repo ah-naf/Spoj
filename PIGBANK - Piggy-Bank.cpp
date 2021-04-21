4b7a7a49c0c8fb9cc55ad51a02e3a7ae7a96e9f3
Ahnaf Hasan Shifat <81911439+ah-naf@users.noreply.github.com>
2021-04-21 22:13:27 +0600

    Create PIGBANK - Piggy-Bank.cpp

---
+++ PIGBANK - Piggy-Bank.cpp
@@ -0,0 +1,75 @@
+/*________________________Bismillahir Rahmanir Rahim___________________
+                                                                      |
+|                    /\     |     | |\    |     /\     |-------        |
+|                   /  \    |     | | \   |    /  \    |               |
+|                  /----\   |-----| |  \  |   /----\   |-----          |
+|                 /      \  |     | |   \ |  /      \  |               |
+|                /        \ |     | |    \| /        \ |               |
+|                     14th batch CSE , ID : 12008029                   |
+|                     Comilla University                               |
+|                     Email : sheikhahnafshifat@gmail.com              |
+|______________________________________________________________________*/
+#include<bits/stdc++.h>
+#include<string>
+#include<iostream>
+using namespace std;
+#define ll                      long long
+#define MAX                     1000007
+#define pb(x)                   push_back(x)
+#define mp(x,y)                 make_pair(x,y)
+#define vii                     vector<int>
+#define vll                     vector<long long>
+#define vp                      vector<pair<ll, ll> >
+#define v_min(a)                *min_element(a.begin(),a.end())
+#define v_max(a)                *max_element(a.begin(),a.end())
+#define v_sum(a)                accumulate(a.begin(),a.end(),0)
+#define un(a)                   a.erase(unique(a.begin(),a.end()),a.end())
+#define SORT(a)                 sort(a.begin(),a.end())
+#define AHNAF                   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define YES                     cout<<"YES"<<endl
+#define NO                      cout<<"NO"<<endl
+#define testcase(t)             int t;cin>>t;while(t--)
+#define reverse(a)              reverse(a.begin(),a.end())
+#define ff                      first
+#define ss                      second
+#define print(a)                for(auto it: a) cout<<it<<" "
+#define all(a)                  a.begin(),a.end()
+#define sf(v)                   for(auto &it:v) cin>>it
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
+void func(int w, vll val, vll wt, int coin) {
+    ll dp[w+3];
+    for(int i=1; i <= w; i++) dp[i] = 99999999;
+    dp[0] = 0;
+    for(int i=1; i<=w; i++) {
+        for(int j=0; j<coin; j++) {
+            if(wt[j] <= i) {
+                dp[i] = min(dp[i], dp[i-wt[j]] + val[j]);
+            }
+        }
+    }
+    if(dp[w] != 99999999) cout<<"The minimum amount of money in the piggy-bank is "<<dp[w]<<"."<<endl;
+    else cout<<"This is impossible."<<endl;
+}
+
+int main()
+{
+    AHNAF;
+    testcase(t) {
+        ll pig, bank, coin;cin>>pig>>bank>>coin;
+        vll val(coin), wt(coin);
+        int weight = bank - pig;
+        for(int i=0; i<coin; i++) cin>>val[i]>>wt[i];
+       // memset(dp, 99999999, sizeof(dp));
+        func(weight, val, wt, coin);
+    }
+}
